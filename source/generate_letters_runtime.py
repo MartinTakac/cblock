5ee83a18015681d370a354f874e27a11743fd53c
Khurram Jawed <khurram@soulmachines.com>
2020-11-09 09:00:56 +1300

    martin_cblock_merge

--- source/generate_letters_runtime.py
+++ source/generate_letters_runtime.py
@@ -2,6 +2,9 @@ from bl_runtime_generator import RuntimeGenerator
 from pathlib import Path
 
 runtime_generator = RuntimeGenerator()
-runtime_generator.generate_runtime(Path("./model_template"),
-                                   Path("./cblock_tests/letters/runtime_data/scene/cblock"),
-                                   Path("./cblock_tests/letters/cblock_letters_config.json"))
+#runtime_generator.generate_runtime(Path("./model_template_2"),
+#                                   Path("./runtime_data/scene/cblock"),
+#                                   Path("./cblock_letters_config.json"))
+runtime_generator.generate_runtime(Path("./model_template_2"),
+                                   Path("./runtime_data/scene/cblock"),
+                                   Path("./cblock_motor_config.json"))
