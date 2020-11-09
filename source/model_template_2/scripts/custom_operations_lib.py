bb1a97151c8746be559213d6fb0f7477ae203abf
Martin Takac <martin.takac@soulmachines.com>
2020-11-09 23:28:00 +0100

    correct scripts for xy_to_loc_som weights

--- source/model_template_2/scripts/custom_operations_lib.py
+++ source/model_template_2/scripts/custom_operations_lib.py
@@ -7,27 +7,27 @@ def calculate_som_weights(som_x, som_y):
     for y in range(0,som_y,1):
         for x in range(0,som_x,1):
             x_output = (x+0.5)/som_x
-            y_output = (y+0.5)/som_y
+            y_output = 1-(y+0.5)/som_y
             list_of_values.append([x_output, y_output])
     return list_of_values
 
 # def calculate_som_weights(som_x, som_y):
-#     dimGranularityX = som_x
-#     dimGranularityY = som_y
-#     minX = -0.03
-#     minY = -0.03
-#     maxX = 1.03
-#     maxY = 1.03
-#     stepX = (maxX-minX)/(dimGranularityX-1)
-#     stepY = (maxY-minY)/(dimGranularityY-1)
+    # dimGranularityX = som_x
+    # dimGranularityY = som_y
+    # minX = -0.03
+    # minY = -0.03
+    # maxX = 1.03
+    # maxY = 1.03
+    # stepX = (maxX-minX)/(dimGranularityX-1)
+    # stepY = (maxY-minY)/(dimGranularityY-1)
 
-#     list_of_values = []
-#     for y in range(0,dimGranularityY,1):
-#         y_output = maxY - stepY*y
-#         for x in range(0,dimGranularityX,1):
-#             x_output = minX + stepX*x
-#             list_of_values.append([x_output, y_output])
-#     return list_of_values
+    # list_of_values = []
+    # for y in range(0,dimGranularityY,1):
+        # y_output = maxY - stepY*y
+        # for x in range(0,dimGranularityX,1):
+            # x_output = minX + stepX*x
+            # list_of_values.append([x_output, y_output])
+    # return list_of_values
 
 def calculate_som_weights_np(som_x, som_y):
     return np.array(calculate_som_weights(som_x, som_y))
